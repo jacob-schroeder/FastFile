606f6a2d7ca81b987081eaea81e2ae1cb78b61c4
jacob <schroeder.m.jacob@gmail.com>
2026-06-02 21:57:08 -0400

    change: add saving of rawfiles.

--- FastFile/UI/Views/Assets/RawfileAssetView.axaml.cs
+++ FastFile/UI/Views/Assets/RawfileAssetView.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using FastFile.Logic.Compression;
 using FastFile.Models.Assets.RawFiles;
+using FastFile.Models.Data;
 using System.Text;
 using UI.Views.Assets.Highlighting;
 
@@ -8,17 +10,38 @@ namespace UI.Views.Assets;
 
 public partial class RawfileAssetView : UserControl
 {
+    private RawFile? _asset;
+
     public RawfileAssetView()
     {
         InitializeComponent();
         InitializeEditor(fileName: null);
+        SaveRawFileButton.IsEnabled = false;
     }
 
     public RawfileAssetView(RawFile asset)
     {
         InitializeComponent();
+        _asset = asset;
         InitializeEditor(asset.Name);
         RawFileTextEditor.Text = ReadFileText(asset);
+    }
+
+    private void SaveRawFileButton_Click(object? sender, RoutedEventArgs e)
+    {
+        if (_asset is null)
+            return;
+
+        var decompressed = Encoding.UTF8.GetBytes(RawFileTextEditor.Text ?? string.Empty);
+        var compressed = ZLib.Compress(decompressed);
+
+        _asset.Len = decompressed.Length;
+        _asset.CompressedLen = compressed.Length;
+
+        if (_asset.BufferPtr is null)
+            _asset.BufferPtr = new ZonePointer<byte[]>(-1);
+
+        _asset.BufferPtr.SetResult(compressed);
     }
 
     private static string ReadFileText(RawFile asset)
